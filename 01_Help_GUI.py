518c76c861247440d129029425d564f2199d518d
natashawensley <wensleyn@middleton.school.nz>
2022-03-28 11:34:17 +1300

    V1 Help GUI

---
+++ 01_Help_GUI.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import random
+
+
+class Convertor:
+    def __int__(self, parent):
+        print("hello world")
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Convertor")
+    something = Convertor(root)
+    root.mainloop()
---
+++ 01_Help_GUIv1.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import random
+
+
+class Convertor:
+    def __int__(self):
+        print("hello world")
+
+    # Formatting variables...
+    background_color = "light blue"
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Convertor")
+    something = Convertor(root)
+    root.mainloop()
