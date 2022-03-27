76b9e98b1f0c05cb1d232b8353abf0222aed9374
natashawensley <wensleyn@middleton.school.nz>
2022-03-28 11:52:41 +1300

    Defined help

--- 01_Help_GUIv1.py
+++ 01_Help_GUIv1.py
@@ -9,9 +9,20 @@ class Convertor:
     # Formatting variables...
     background_color = "light blue"
 
+    #Converter Main Screen GUI...
+    self.converter_frame = Frame(width=300, height=300, bg=background_color)
+    self.converter_frame.grid()
+
+    # Temperature Conversion Heading (row 0)
+    self.temp_converter_label = Label(text="Temperature Converter",
+                                      font=("Arial, "16", "bold"),
+                                      bg=background_color,
+                                      padx=10, payd=10)
+    self.temp_converter_label.grid(row=0))
+
 # main routine
 if __name__ == "__main__":
     root = Tk()
     root.title("Temperature Convertor")
-    something = Convertor(root)
+    something = Convertor()
     root.mainloop()
---
+++ 01_Help_GUIv2.py
@@ -0,0 +1,39 @@
+from tkinter import *
+
+
+class Convertor:
+    def __int__(self):
+
+    # Formatting variables...
+    background_color = "light blue"
+
+    #Converter Main Screen GUI...
+    self.converter_frame = Frame(width=300, height=300,
+                                 bg=background_color, pady =10)
+    self.converter_frame.grid()
+
+    # Temperature Conversion Heading (row 0)
+    self.temp_converter_label = Label(self.converter_frame
+                                      text="Temperature Converter",
+                                      font=("Arial, "16", "bold"),
+                                      bg=background_color,
+                                      padx=10, payd=10)
+    self.temp_converter_label.grid(row=0))
+
+    # Help button (row 1)
+    self.help_button = Button(self.converter_frame, text="Help",
+                              font=("Arial", "14"),
+                              padx=10, pady=10, command=self.help)
+    self.help_button.grid(row=1)
+
+    def help(self):
+        print("You asked for help")
+        get_help = Help()
+        get_help.help_text.configure(text"Help text goes here")
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Convertor")
+    something = Convertor()
+    root.mainloop()
